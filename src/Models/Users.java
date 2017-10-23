bea1addb61ed45eb4fe0c6ce35fb4d5a5c8c53a1
Damian <Damian@192.168.0.17>
2017-10-23 20:08:35 +0100

    Starting of the Assignment

---
+++ src/Models/Users.java
@@ -0,0 +1,24 @@
+package Models;
+
+public class Users {
+	public String fName;
+	public String lName;
+	public String age;
+	public String gender;
+	public String occupation;
+	public String zipCode;
+	
+	public Users() {
+		
+}
+
+	public Users(String fName, String lName, String age, String gender, String occupation, String zipCode) {
+		this.fName = fName;
+		this.lName = lName;
+		this.age = age;
+		this.gender = gender;
+		this.occupation = occupation;
+		this.zipCode = zipCode;
+	}
+	
+}
