8cfe0a633d947867f3179d89e307c530e34708ee
Damian <Damian@192.168.0.17>
2017-10-23 20:22:12 +0100

    Xstream Added to convert into a XML stream

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,14 +1,20 @@
 package controllers;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import Models.Users;
 import utilities.FileLogger;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		FileLogger logger = FileLogger.getLogger();
 		logger.log("Creating user list");
@@ -18,6 +24,12 @@ public class Main {
 		users.add(new Users("Sylwia","Mamak","41","F","Manager","E91W594"));
 		users.add(new Users("Robert","Mamak","46","M","Builder","E91W594"));
 		System.out.println(users);
+		
+		logger.log("Serializing contacts to XML");
+		XStream xstream = new XStream(new DomDriver());
+		ObjectOutputStream out= xstream.createObjectOutputStream(new FileWriter("Users.xml"));
+		out.writeObject(users);
+		out.close();
 		
 		logger.log("Finished - Shutting down");
 
